96f11d2ba6f7e496a11881b6783c65c0331017d4
Jake Mills <jake.mills.05@gmail.com>
2020-07-05 20:33:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,6 +20,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
+    elsif value == {}
+      nil
     end
     if lowest_value == value
       lowest_key = key
